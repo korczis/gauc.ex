5aee4f9651ae30fc03d1210336fc3accbfff8b29
Tomas Korcak <korczis@gmail.com>
2017-10-03 20:33:23 +0200

    Initial commit

---
+++ lib/gauc.ex
@@ -0,0 +1,18 @@
+defmodule Gauc do
+  @moduledoc """
+  Documentation for Gauc.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Gauc.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/gauc_test.exs
@@ -0,0 +1,8 @@
+defmodule GaucTest do
+  use ExUnit.Case
+  doctest Gauc
+
+  test "greets the world" do
+    assert Gauc.hello() == :world
+  end
+end
